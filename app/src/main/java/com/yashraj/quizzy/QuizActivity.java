5f65eead18db9b3b47438b8c8eae992294bc73d8
yashraj <yashraj.verma_cs18@gla.ac.in>
2020-09-16 22:22:21 +0530

    Initial commit

--- app/src/main/java/com/yashraj/quizzy/QuizActivity.java
+++ app/src/main/java/com/yashraj/quizzy/QuizActivity.java
@@ -1,15 +1,40 @@
 package com.yashraj.quizzy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class QuizActivity extends AppCompatActivity {
+    BottomNavigationView.OnNavigationItemSelectedListener navlistner=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.category:
+                    selectedFragment = new CategoryFragment();
+                    break;
+                case R.id.ranking:
+                    selectedFragment = new RankingFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selectedFragment).commit();
+            return true;
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizactivity);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_appBar);
+        bottomNav.setOnNavigationItemSelectedListener(navlistner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new CategoryFragment()).commit();
 
     }
 }
